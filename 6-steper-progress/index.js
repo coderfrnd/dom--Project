56f55676ceb6ab00b953a6660d176374ae9741ec
Abhishek <warriorsshadow39@gmail.com>
2025-02-27 20:54:34 +0530

    done both projects

--- 6-steper-progress/index.js
+++ 6-steper-progress/index.js
@@ -8,68 +8,58 @@ let msgArr = [
   "Your  Payment method",
   "Your Product Delivered",
 ];
-
 arr = Array.from(arr);
 colorBar.style.backgroundColor = "red";
 let width = "34%";
 let value = 0;
 let counter = 0;
-let next = document.getElementById("next");
-let back = document.getElementById("back");
-function increaseProgress(next) {
-  next.addEventListener("click", () => {
-    if (parseInt(value) <= 70) {
-      value = parseInt(value) + parseInt(width);
-      value = value + "%";
-      colorBar.style.width = value;
-      arr[counter].innerHTML = "&#10003";
-      arr[counter].style.backgroundColor = "green";
-      if (counter < 3) {
-        arr[counter + 1].style.backgroundColor = "blue";
-        message.textContent = msgArr[counter + 1];
-      }
+let nextBtn = document.getElementById("next");
+let backBtn = document.getElementById("back");
 
-      counter++;
-    } else {
-      if (counter == 3) {
-        arr[counter].innerHTML = "&#10003";
-        arr[counter].style.backgroundColor = "green";
-        counter++;
-        console.log(value);
-      }
-    }
-  });
-}
+nextBtn.addEventListener("click", () => {
+  if (arr[counter - 1]) {
+    arr[counter - 1].style.backgroundColor = "Green";
+  }
+  if (arr[counter]) {
+    arr[counter].style.backgroundColor = "blue";
+    message.textContent = msgArr[counter];
+    counter++;
+  }
+  if (parseInt(value) <= 102) {
+    colorBar.style.width = value;
+    value = parseInt(value) + parseInt(width);
+    value = value + "%";
+    console.log(value);
+  }
 
-function decrease(back) {
-  back.addEventListener("click", () => {
-    if (parseInt(value) >= 34) {
-      console.log(value);
+  if (counter >= 4) {
+    nextBtn.disabled = true;
+  }
+  if (counter > 0) {
+    backBtn.disabled = false;
+  }
+});
 
-      colorBar.style.width = value;
-      arr[counter - 1].textContent = counter;
-      arr[counter - 1].style.backgroundColor = "blue";
-      if (counter <= 3) {
-        arr[counter].style.backgroundColor = "grey";
-        message.textContent = msgArr[counter - 1];
-      }
-      counter--;
-      value = parseInt(value) - parseInt(width);
-      value = value + "%";
-    } else if (counter == 1) {
-      colorBar.style.width = value;
-      arr[counter].style.backgroundColor = "grey";
-      console.log(value);
-      arr[counter - 1].textContent = counter;
-      arr[counter - 1].style.backgroundColor = "blue";
-      message.textContent = msgArr[counter - 1];
-      counter--;
-    } else {
-      arr[counter].style.backgroundColor = "grey";
-    }
-  });
-}
-increaseProgress(next);
-decrease(back);
+backBtn.addEventListener("click", () => {
+  if (arr[counter]) {
+    console.log(value);
+    arr[counter].style.backgroundColor = "grey";
+  }
+  if (arr[counter - 1]) {
+    arr[counter - 1].style.backgroundColor = "blue";
+    message.textContent = msgArr[counter - 1];
+    counter--;
+  }
+  if (parseInt(value) > 0) {
+    value = parseInt(value) - parseInt(width);
+    value = value + "%";
+    colorBar.style.width = value;
+  }
+  if (counter <= 3) {
+    nextBtn.disabled = false;
+  }
 
-// &#10003;
+  if (counter == 0) {
+    backBtn.disabled = true;
+  }
+});
--- 7-transfer-list/index.js
+++ 7-transfer-list/index.js
@@ -1,60 +1,59 @@
-let leftContainer = document.querySelectorAll("#left");
-let rightContainer = document.querySelectorAll("#right");
+let leftContainer = document.querySelector("#left");
+let rightContainer = document.querySelector("#right");
 let buttonContainer = document.querySelectorAll("#center");
-let leftList = leftContainer[0].children[0];
-let rightList = rightContainer[0].children[0];
+let leftList = leftContainer.children[0];
+let rightList = rightContainer.children[0];
 let moveAllToLeftBtn = document.getElementById("first");
 let moveSelectedToLeftBtn = document.getElementById("second");
 let moveAllToRightBtn = document.getElementById("third");
 let moveSelectedToRightBtn = document.getElementById("fourth");
+let parentContainer = document.getElementById("main");
 
-let leftItems = Array.from(leftList.children);
+moveAllToLeftBtn.addEventListener("click", moveAllToLeft);
+moveSelectedToLeftBtn.addEventListener("click", moveSelectedToLeft);
+moveAllToRightBtn.addEventListener("click", moveAllToRight);
+moveSelectedToRightBtn.addEventListener("click", moveSelectedToRight);
 
-function moveAllToLeft() {
-  moveAllToLeftBtn.addEventListener("click", () => {
-    let rightItems = Array.from(rightList.children);
-    rightItems.forEach((item) => {
-      item.children[0].checked = false;
-      leftList.appendChild(item);
-    });
-  });
-}
-
-function moveSelectedToLeft() {
-  moveSelectedToLeftBtn.addEventListener("click", () => {
-    let rightItems = Array.from(rightList.children);
-    rightItems.forEach((item) => {
-      if (item.children[0].checked) {
-        item.children[0].checked = false;
-        leftList.appendChild(item);
-      }
-    });
-  });
-}
+parentContainer.addEventListener("click", () => {
+  let checkedLeft = leftContainer.querySelector("input:checked");
+  let checkedRight = rightContainer.querySelector("input:checked");
+  if (checkedLeft) {
+    moveSelectedToRightBtn.disabled = false;
+  }
+  if (checkedRight) {
+    moveSelectedToLeftBtn.disabled = false;
+  }
+});
 
 function moveSelectedToRight() {
-  moveSelectedToRightBtn.addEventListener("click", () => {
-    let leftItems = Array.from(leftList.children);
-    leftItems.forEach((item) => {
-      if (item.children[0].checked) {
-        item.children[0].checked = false;
-        rightList.appendChild(item);
-      }
-    });
-  });
-}
-
-function moveAllToRight() {
-  moveAllToRightBtn.addEventListener("click", () => {
-    let leftItems = Array.from(leftList.children);
-    leftItems.forEach((item) => {
+  let leftItems = Array.from(leftList.children);
+  leftItems.forEach((item) => {
+    if (item.children[0].checked) {
       item.children[0].checked = false;
       rightList.appendChild(item);
-    });
+    }
   });
 }
-
-moveAllToLeft();
-moveSelectedToLeft();
-moveAllToRight();
-moveSelectedToRight();
+function moveAllToLeft() {
+  let rightItems = Array.from(rightList.children);
+  rightItems.forEach((item) => {
+    item.children[0].checked = false;
+    leftList.appendChild(item);
+  });
+}
+function moveSelectedToLeft() {
+  let rightItems = Array.from(rightList.children);
+  rightItems.forEach((item) => {
+    if (item.children[0].checked) {
+      item.children[0].checked = false;
+      leftList.appendChild(item);
+    }
+  });
+}
+function moveAllToRight() {
+  let leftItems = Array.from(leftList.children);
+  leftItems.forEach((item) => {
+    item.children[0].checked = false;
+    rightList.appendChild(item);
+  });
+}
